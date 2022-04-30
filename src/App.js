5882dc2087299830a4457467a46769ad822cb54b
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-04-30 20:07:53 +0600

    header footer login UI done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './HomePage/Footer/Footer';
+import Header from './HomePage/Header/Header';
+import Login from './Pages/Register/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Login></Login>
+      <Footer></Footer>
     </div>
   );
 }
