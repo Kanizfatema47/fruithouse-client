5f89ae6d22e6d5b6dd2185fe5504619c86b29aa7
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-05-02 08:13:41 +0600

    banner and product section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Banner from './HomePage/Banner/Banner';
 import Footer from './HomePage/Footer/Footer';
 import Header from './HomePage/Header/Header';
+import Products from './Pages/Products/Products';
 import Login from './Pages/Register/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
+      <Products></Products>
       <Login></Login>
       <Footer></Footer>
     </div>
--- src/Pages/Register/Login/Login.js
+++ src/Pages/Register/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LockClosedIcon } from '@heroicons/react/outline';
+import { Link } from 'react-router-dom';
 const Login = () => {
     return (
         <>
@@ -20,12 +21,7 @@ const Login = () => {
               alt="Workflow"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{' '}
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                start your 14-day free trial
-              </a>
-            </p>
+           
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
@@ -79,7 +75,9 @@ const Login = () => {
                 </a>
               </div>
             </div>
-
+              <div>
+                <p>Need Register? <Link className="font-medium text-indigo-600 hover:text-indigo-500" to='/register'>Register</Link></p>
+              </div>
             <div>
               <button
                 type="submit"
